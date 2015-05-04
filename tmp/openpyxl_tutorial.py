0fb52e1b933e885dffda14b8001c2a7982a8dd1f
Theodoros Stylianos Kondylis <kondil@gmail.com>
2015-05-04 16:16:00 +0200

    I moved on a little bit with the openpyxl tutorial. Now I can read the file but I need to find how to handle the values. And finally how to save this to a report.xlsx .

--- tmp/openpyxl_tutorial.py
+++ tmp/openpyxl_tutorial.py
@@ -8,11 +8,27 @@
 from openpyxl import load_workbook
 
 wb = load_workbook(filename='Nov.xlsx', read_only=True)
-ws = wb['Nov.csv']
 
-print ws.rows
+# In order to discover more info on the module, classes and objects we have loaded 
+# we can use the commands dir() and help() like the next examples.
+# dir(wb)
+# help(wb)
+# help(wb.get_active_sheet)
 
-# This will print the help.
-help(ws.rows)
+
+# ws = wb['Nov.csv']
+# The following command will get us the active sheet of the workbook.
+ws = wb.get_active_sheet()
+
+# print ws.rows
+print ws.dimensions
+print ws.max_column
+print ws.max_row
+
+for row in ws.rows:
+	print "=======================================\nNext Entry\n======================================="
+	for cell in row:
+		print cell.value
+	print "=======================================\n"
 
 
