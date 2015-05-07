832fcc858eb878061a92d8a110c586ef79a5064e
Theodoros Stylianos Kondylis <kondil@gmail.com>
2015-05-07 10:18:57 +0200

    A startup test on Arguments' passing to python scripts.

---
+++ tmp/pass_arguments.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# This is a test on passing arguments to our python scripts.
+
+import sys
+
+print 'Num of args :: ', len(sys.argv)
+print 'Argument List :: ', str(sys.argv)
+print 'Argument List :: ', sys.argv
+
+
+# For now I will use it as is. For further info one can visit :: 
+# http://www.tutorialspoint.com/python/python_command_line_arguments.htm
