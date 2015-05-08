1306581bc0314e336a17e5268214037eec9cdf11
Theodoros Stylianos Kondylis <kondil@gmail.com>
2015-05-08 16:32:24 +0200

    Some steps in argument passing. Not ready yet.

--- tmp/pass_arguments.py
+++ tmp/pass_arguments.py
@@ -4,38 +4,45 @@
 
 import sys, getopt
 
-print 'Num of args :: ', len(sys.argv)
-print 'Argument List :: ', str(sys.argv)
-print 'Argument List :: ', sys.argv
-print type(sys.argv[1])
+# print 'Num of args :: ', len(sys.argv)
+# print 'Argument List :: ', str(sys.argv)
+# print 'Argument List :: ', sys.argv
+# print type(sys.argv[1])
+# 
+# # For now I will use it as is. For further info one can visit :: 
+# # http://www.tutorialspoint.com/python/python_command_line_arguments.htm
 
-# For now I will use it as is. For further info one can visit :: 
-# http://www.tutorialspoint.com/python/python_command_line_arguments.htm
+def help_message(scripts_name):
+	# print "Input function was called."
+	print scripts_name, ' -i <inputfile> -o <outputfile>'
 
-
-def input(argv):
-
-	print "Input function was called."
-
+def input(argv, inputFile, outputFile):
 	inputFile = ''
 	outputFile = ''
 
 	try:
-		opts, args = getopt.getopt(argv,"hi:o:",["help","input=","output="])
+		opts, args = getopt.getopt(argv[1:],"hi:o:",["help","input=","output="])
 	except getopt.GetoptError:
-		print 'test.py -i <inputfile> -o <outputfile>'
+		print help_message(argv[0])
 		sys.exit(2)
 
  	for opt, arg in opts:
  		if opt in ("-h", "--help"):
- 			print 'test.py -i <inputfile> -o <outputfile>'
+ 			help_message(argv[0])
  			sys.exit()
  		elif opt in ("-i", "--input"):
  			inputFile = arg
  		elif opt in ("-o", "--output"):
  			outputFile = arg
- 			print 'Input file is "', inputFile
- 			print 'Output file is "', outputFile
+
+def main(argv):
+# 	inputFile = None
+# 	outputFile = None
+
+	input(argv, inputFile, outputFile)
+	print 'Input file is ', inputFile
+	print 'Output file is ', outputFile
 
 
-input(sys.argv[1:])
+if __name__ == "__main__":
+	main(sys.argv)
