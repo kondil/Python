0b17a5d9377d321d84093f968dfc07fc4bf0d211
Theodoros Stylianos Kondylis <kondil@gmail.com>
2015-10-09 10:54:48 +0200

    Changes to write bold.

--- 101_Openpyxl/write_xlsx.py
+++ 101_Openpyxl/write_xlsx.py
@@ -2,7 +2,7 @@
 # This is a tutorial on how to create an xlsx Workbook using the Openpyxl library.
 # All infos retrieved from :: 
 # https://openpyxl.readthedocs.org/en/latest/tutorial.html#create-a-workbook
-from openpyxl import Workbook
+from openpyxl import Workbook, styles
 
 # Let's create a Workbook.
 wb = Workbook()
@@ -14,10 +14,13 @@ ws.append([1,2,3])
 ws = wb.create_sheet(0, 'tmp')
 wb.get_sheet_names()
 
+font = styles.Font(bold=True)
+
 for row in range(ord('A'), ord('A')+15):
 	for col in range(1,38):
 		cell_pos = chr(row)+str(col)	# print cell_pos
 		ws.cell(coordinate=cell_pos, value=cell_pos)
+		ws[cell_pos].font = font
 		# This one did not work for me :: ws.cell(row=row, column=col, value=cell_pos)
 		# This is one is not working :: ws.cell(cell_pos, cell_pos)
 
