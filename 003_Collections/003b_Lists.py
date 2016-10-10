93929017b2217df4d59aa96895b5d93e436f4129
Theodoros Stylianos Kondylis <kondil@gmail.com>
2016-10-10 17:27:43 +0200

    Small changes in the lists

--- 003_Collections/003b_Lists.py
+++ 003_Collections/003b_Lists.py
@@ -5,6 +5,7 @@
 # - They are mutables and there methods to modify the lists.
 # - They are created using square brackets.
 
+print '>> fruit = ["Apple", "Hawthorn", "Loquat", "Medlar", "Pear", "Quince"]'
 fruit = ["Apple", "Hawthorn", "Loquat", "Medlar", "Pear", "Quince"]
 print "fruit = ", fruit
 print "fruit[:2] = ", fruit[:2]
@@ -20,10 +21,23 @@ print fruit
 fruit[4:4] = ["Rowan"]
 print "Added a fruit using slicing. ", fruit
 
+print '>> fruit[4:5] = []'
 fruit[4:5] = []
 print "Deleted the fruit using slicing. ", fruit
 
 # Append in the end of the list.
+print '>> fruit.append("Watermelon")'
 fruit.append("Watermelon")
 print fruit
+
+print '>> fruit.insert(7,"NooN")'
+fruit.insert(7,"NooN")
+print fruit
+
+print '>> fruit.insert(7,"NaaN")'
+fruit.insert(7,"NaaN")
+print fruit
+
+print '>> print len(fruit)'
+print len(fruit)
 
